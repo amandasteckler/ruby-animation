056bf758a9688ebbf81c4a7e31ce79fa8761bac3
Amanda Steckler <amandajsteckler@gmail.com>
2016-10-19 23:44:09 -0400

    Created animation with ruby friends (who also dance)

---
+++ animation.rb
@@ -0,0 +1,356 @@
+system "clear"
+
+# friend1 walks in from right
+
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "|o o| "
+puts "  u   "
+puts " | |  "
+puts "  |   "
+puts " d b  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "    |o o| "
+puts "      u   "
+puts "     //  "
+puts "      |   "
+puts "     b d  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "        |o o| "
+puts "          u   "
+puts "         | |  "
+puts "          |   "
+puts "         d b  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "            |o o| "
+puts "              u   "
+puts "             //  "
+puts "              |   "
+puts "             b d  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                |o o| "
+puts "                  u   "
+puts "                 | |  "
+puts "                  |   "
+puts "                 d b  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                    |o o| "
+puts "                      u   "
+puts "                     //  "
+puts "                      |   "
+puts "                     b d  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                        |o o| "
+puts "                          u   "
+puts "                         | |  "
+puts "                          |   "
+puts "                         d b  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                            |o o| "
+puts "                              u   "
+puts "                             //   "
+puts "                              |   "
+puts "                             b d  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o| "
+puts "                                  u   "
+puts "                                 | |  "
+puts "                                  |   "
+puts "                                 d b  "
+
+
+
+
+# friend2 walks in from right
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|                                   |o o| "
+puts "                                  u                                       u   "
+puts "                                 | |                                     | |  "
+puts "                                  |                                       |   "
+puts "                                 d b                                     d b  "
+
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|                               |o o| "
+puts "                                  u                                   u   "
+puts "                                 | |                                 //   "
+puts "                                  |                                   |   "
+puts "                                 d b                                 b d  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|                           |o o| "
+puts "                                  u                               u   "
+puts "                                 | |                             | |  "
+puts "                                  |                               |   "
+puts "                                 d b                             d b  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|                       |o o| "
+puts "                                  u                           u   "
+puts "                                 | |                         //   "
+puts "                                  |                           |   "
+puts "                                 d b                         b d  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|                   |o o| "
+puts "                                  u                       u   "
+puts "                                 | |                     | |  "
+puts "                                  |                       |   "
+puts "                                 d b                     d b  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|               |o o| "
+puts "                                  u                   u   "
+puts "                                 | |                 //   "
+puts "                                  |                   |   "
+puts "                                 d b                 b d  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|           |o o| "
+puts "                                  u               u   "
+puts "                                 | |             | |  "
+puts "                                  |               |   "
+puts "                                 d b             d b  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|       |o o| "
+puts "                                  u           u   "
+puts "                                 | |         //   "
+puts "                                  |           |   "
+puts "                                 d b         b d  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |o o|    Hey."
+puts "                                  u       u   "
+puts "                                 | |     | |  "
+puts "                                  |       |   "
+puts "                                 d b     d b  "
+
+sleep 1
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |o o| "
+puts "                                  u       u   "
+puts "                                 | |     | |  "
+puts "                                  |       |   "
+puts "                                 d b     d b  "
+
+sleep 2
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |o o|    Hello?"
+puts "                                  u       -   "
+puts "                                 | |     | |  "
+puts "                                  |       |   "
+puts "                                 d b     d b  "
+
+sleep 1
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |o o| "
+puts "                                  u       -   "
+puts "                                 | |     | |  "
+puts "                                  |       |   "
+puts "                                 d b     d b  "
+
+sleep 2
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |_ _| "
+puts "                                  u       -'  "
+puts "                                 | |     | |  "
+puts "                                  |       |   "
+puts "                                 d b     d b  "
+
+sleep 0.25
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |_ _| "
+puts "                                  u       -   "
+puts "                                 | |     | |' "
+puts "                                  |       |   "
+puts "                                 d b     d b  "
+
+sleep 0.25
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |_ _| "
+puts "                                  u       -   "
+puts "                                 | |     | |  "
+puts "                                  |       | ' "
+puts "                                 d b     d b  "
+
+sleep 0.25
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |_ _| "
+puts "                                  u      '-   "
+puts "                                 | |     | |  "
+puts "                                  |       |   "
+puts "                                 d b     d b' "
+
+sleep 0.25
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |_ _| "
+puts "                                  u       -   "
+puts "                                 | |    '| |  "
+puts "                                  |       |   "
+puts "                                 d b     d b  "
+
+sleep 0.25
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |_ _| "
+puts "                                  u       -'  "
+puts "                                 | |     | |  "
+puts "                                  |     ' |   "
+puts "                                 d b     d b  "
+
+sleep 0.25
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |_ _| "
+puts "                                  u       -   "
+puts "                                 | |     | |' "
+puts "                                  |       |   "
+puts "                                 d b    'd b  "
+
+sleep 0.25
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |_ _| "
+puts "                                  u       -   "
+puts "                                 | |     | |  "
+puts "                                  |       | ' "
+puts "                                 d b     d b  "
+
+sleep 0.25
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                                |o o|   |_ _| "
+puts "                                  u       -   "
+puts "                                 | |     | |  "
+puts "                                  |       |   "
+puts "                                 d b     d b' "
+
+
+sleep 0.25
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                     OH HI!!    |o o|   |_ _| "
+puts "                                  u       -   "
+puts "                                 | |     | |  "
+puts "                                  |       |   "
+puts "                                 d b     d b  "
+
+sleep 0.5
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                     OH HI!!    |o o|   |o o| "
+puts "                                  u       -   "
+puts "                                 | |     | |  "
+puts "                                  |       |   "
+puts "                                 d b     d b  "
+
+sleep 1
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                     OH HI!!    |o o|   |o o| "
+puts "                                  u       u   "
+puts "                                 | |     | |  "
+puts "                                  |       |   "
+puts "                                 d b     d b  "
+
+sleep 1
+system "clear"
+
+puts "\n \n \n \n \n        F  R  I  E  N  D  S    (W  H  O    A  L  S  O    D  A  N  C  E) \n \n \n \n \n"
+puts "                     OH HI!!    |o o|   |O O|    Yay!"
+puts "                                  u       U   "
+puts "                                 | |     \\ /  "
+puts "                                  |       |   "
+puts "                                 d b     d b  "
+
+
+sleep 5
+
+
+
+
+
